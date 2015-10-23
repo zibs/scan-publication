f7edc21a96041077c1cbb0af39c4d86ea2116a5c
zibs <elizibin@gmail.com>
2015-10-23 13:16:58 -0700

    add about section

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -4,8 +4,4 @@ class PublicationsController < ApplicationController
   	@issues = Issue.order(created_at: :asc)
   end
 
-  def download(issue)
-    send_file issue.document.path, :type => 'application/pdf', :filename => issue.permalink
-  end
-
 end
